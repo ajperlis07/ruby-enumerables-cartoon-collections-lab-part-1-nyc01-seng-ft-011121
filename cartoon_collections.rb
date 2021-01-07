59e738dcf4079b099f011b901d393487d2b728c3
Andrew  Perlis <ajperlis07@gmail.com>
2021-01-07 18:49:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 def greet_characters(array)
   greet_characters = []
-  greet_characters.each {|character| puts "Hello #{"
-   puts "Hello #{greet_character}!"
+  greet_characters.each {|character| puts "Hello #{character}"
   end
   # Use `each` to enumerate over the provided array
   #
